7e38081f511b05b3ec8dd3304bd0b82cc7809f53
ytia0984 <ytia0984@uni.sydney.edu.au>
2026-01-22 17:00:42 +0800

    fixing

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Publication from '../views/Publication.vue'
 import CV from '../views/CV.vue'
@@ -39,7 +39,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
